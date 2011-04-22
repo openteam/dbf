720f8247d55ac4fe4850db628311d8c3c970de1b
Dmitry Lihachev <lda@openteam.ru>
2011-04-22 17:54:34 +0700

    @memo can be nil in some cases

--- lib/dbf/record.rb
+++ lib/dbf/record.rb
@@ -1,14 +1,14 @@
 module DBF
-  # An instance of DBF::Record represents a row in the DBF file 
+  # An instance of DBF::Record represents a row in the DBF file
   class Record
     # Initialize a new DBF::Record
-    # 
+    #
     # @param [DBF::Table] table
     def initialize(data, columns, version, memo)
       @data, @columns, @version, @memo = StringIO.new(data), columns, version, memo
       define_accessors
     end
-    
+
     # Equality
     #
     # @param [DBF::Record] other
@@ -16,14 +16,14 @@ module DBF
     def ==(other)
       other.respond_to?(:attributes) && other.attributes == attributes
     end
-    
+
     # Maps a row to an array of values
-    # 
+    #
     # @return [Array]
     def to_a
       @columns.map { |column| attributes[column.name.underscore] }
     end
-    
+
     # Do all search parameters match?
     #
     # @param [Hash] options
@@ -31,19 +31,19 @@ module DBF
     def match?(options)
       options.all? {|key, value| attributes[key.to_s.underscore] == value}
     end
-    
+
     def attributes
       return @attributes if @attributes
-      
+
       @attributes = Attributes.new
       @columns.each do |column|
         @attributes[column.name] = init_attribute(column)
       end
       @attributes
     end
-    
+
     private
-    
+
     def define_accessors #nodoc
       @columns.each do |column|
         unless self.class.method_defined? column.underscored_name
@@ -55,11 +55,11 @@ module DBF
         end
       end
     end
-    
+
     def init_attribute(column) #nodoc
-      column.memo? ? @memo.get(get_memo_start_block(column)) : column.type_cast(unpack_data(column))
+      column.memo? && @memo ? @memo.get(get_memo_start_block(column)) : column.type_cast(unpack_data(column))
     end
-   
+
     def get_memo_start_block(column) #nodoc
       if %w(30 31).include?(@version)
         @data.read(column.length).unpack('V').first
@@ -71,6 +71,6 @@ module DBF
     def unpack_data(column) #nodoc
       @data.read(column.length).unpack("a#{column.length}").first
     end
-    
+
   end
 end
